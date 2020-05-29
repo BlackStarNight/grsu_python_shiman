ad031432b5ee4bf69f994b324b351ed7d08cb3c2
Artyom <artemshimon2000@gmail.com>
2020-05-29 15:28:40 +0300

    add lab4 and lab5

---
+++ Lab4_python/Финалка/simple_match.py
@@ -0,0 +1,68 @@
+# Для каждого регулярного выражения, которое требуется написать,
+# указаны строки, соответствующие этому выражению (они отмечены знаком +),
+# а также строки, не соответствующие этому выражению (отмечены знаком -)
+
+# + a
+# + ab
+# - b
+# - ba
+REGEXP_1 = 'ab?'
+
+# + aab
+# + abb
+# + acb
+# - ab
+# - aabc
+REGEXP_2 = 'a[a-c]b$'
+
+# + sofia.mp3
+# + sofia.mp4
+# - sofia.mp7
+# - sofia.mp34
+REGEXP_3 = r'sofia\.mp[3-4]$'
+
+# + taverna
+# + versus
+# + vera
+# + zveri
+# - zver
+REGEXP_4 = '[t-z]?.?ver.+'
+
+# - a
+# - aa
+# + aaa
+# - aaaa
+# - b
+# - bb
+# + bbb
+# - bbbb
+# - ccc
+REGEXP_5 = '^[a-b]{3}$'
+
+# - Ok
+# - OkOk
+# + OkOkOk
+# - OkOkOkOk
+# - ab
+# - abab
+# + ababab
+# - abababab
+REGEXP_6 = '(^(Ok){3}$)|(^(ab){3}$)'
+
+# - aaa
+# - aaa aaa
+# + aaa Aaa aaa
+# + aaa aaa Aaa
+# + Aaa aaa aaa
+# - A
+# - aaa A aaa
+REGEXP_7 = r'^(([Aa]{3}\s)){2}([Aa]{3})'
+
+# + abc
+# + abc03
+# + a-b-c-3
+# + a.b.c.0
+# - Aabc
+# - abc1
+# - #abc
+REGEXP_8 = r'a[bc\.\-]*[03]*$'
